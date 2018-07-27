9596c5f0ac0c4c45e93101c87272229db4fc358c
KAZUE ANAN <zakscloset@gmail.com>
2018-07-27 13:26:45 -0400

    gradient

--- src/js/datafeed.js
+++ src/js/datafeed.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const lookupData = axios.get('/lookup')
 .then(data => data)
 
-export const historicalData = axios.get('/historical')
-.then(data => data)
+// export const historicalData = axios.get('/historical')
+// .then(data => data)
 
 
--- src/js/index.js
+++ src/js/index.js
@@ -6,15 +6,19 @@ const lookup = lookupData.then(data => {
 })
 
 const scene = new THREE.Scene();
+// scene.background = new THREE.Color(0xffff0)
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ alpha: true });
+renderer.setClearColor(0x000000, 0.0);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.SphereGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xffff00, wireframe: true }) 
+
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh)
+
 camera.position.z = 5;
 
 const animate = () => {
