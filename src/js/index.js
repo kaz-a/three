99ad99d2b39c93be85048dcba75c5ca108618dbc
KAZUE ANAN <zakscloset@gmail.com>
2018-07-28 23:10:43 -0400

    css

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,8 @@ const lookup = lookupData.then(data => {
   console.log('lookup:', data.data)
 })
 
+
 const scene = new THREE.Scene();
-// scene.background = new THREE.Color(0xffff0)
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setClearColor(0x000000, 0.0);
@@ -15,9 +15,8 @@ document.body.appendChild(renderer.domElement);
 
 const geometry = new THREE.SphereGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xffff00, wireframe: true }) 
-
 const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh)
+scene.add(mesh);
 
 camera.position.z = 5;
 
