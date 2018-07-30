c813d37b557e26b9f70b1a2ba262dcff13fbb867
KAZUE ANAN <zakscloset@gmail.com>
2018-07-30 01:40:31 -0400

    spheres

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -10,37 +10,101 @@ class SomeScene extends Component {
   three(){
     const {data} = this.props;
     console.log('data:', data)
-    
+       
+    // Scene, camera, camera position
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 0.2, 25000);
+    camera.position.set(100, -400, 2000);
+    scene.add(camera)
+
+    // Render
     const renderer = new THREE.WebGLRenderer({ alpha: true });
-    // renderer.setClearColor(0x000000, 0.0);
+    renderer.setClearColor(0x000000, 0.0);
     renderer.setSize(window.innerWidth, window.innerHeight);
     document.getElementById('root').appendChild(renderer.domElement);
 
-    const geometry = new THREE.SphereGeometry(data[0].currentPrice/100, 1, 1);
-    const material = new THREE.MeshBasicMaterial({ color: 0xffff00, wireframe: true }) 
-    const mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh)
+    // Lighting
+    const light = new THREE.PointLight(0xffffff, 1, 4000);
+    light.position.set(50, 0, 0);
+    const light_two = new THREE.PointLight(0xffffff, 1, 4000);
+    light_two.position.set(-100, 800, 800);
+    const lightAmbient = new THREE.AmbientLight(0x404040);
+    scene.add(light, light_two, lightAmbient);
 
-    camera.position.z = 10;
+    // Objects
+    const createSpheres = () => {
+      //create empty 3d object — group for future spheres
+      const spheres = new THREE.Object3D();
+      
+      // create spheres based on data
+      for (let i = 0; i < data.length; i++) {
+        let sphere = new THREE.SphereGeometry(data[i].currentPrice/100, 8, 8);
+        let material = new THREE.MeshBasicMaterial({
+            color: 0xffff00,
+            wireframe: true
+        })
+      
+          //creating the mesh and add primitive and material
+          let particle = new THREE.Mesh(sphere, material);
+          
+          //randomly set position and scale
+          const distance = 400;
+          particle.position.x = Math.random() * distance * 10;
+          particle.position.y = Math.random() * -distance * 6;
+          particle.position.z = Math.random() * distance * 4;
+          particle.rotation.y = Math.random() * 2 * Math.PI;
+          particle.scale.x = particle.scale.y = particle.scale.z = Math.random() * 12 + 5;
 
-    const animate = () => {
-      requestAnimationFrame(animate);
+          //add particle to the spheres group
+          spheres.add(particle);
+        }
+      
+        //correct spheres position relative to the camera
+        spheres.position.y = 500;
+        spheres.position.x = -2000;
+        spheres.position.z = -100;
+        spheres.rotation.y = Math.PI * 600;
 
-      mesh.rotation.x += 0.01;
-      mesh.rotation.y += 0.01;
-
-      renderer.render(scene, camera);
+        //add spheres to the scene
+        scene.add(spheres);
     }
 
-    animate();
+    createSpheres();
+
+    renderer.render(scene, camera)
+
+    // const geometryCOF = new THREE.SphereGeometry(data[2].currentPrice/100, 1, 1);
+    // const materialCOF = new THREE.MeshBasicMaterial({ color: 0xffff00, wireframe: true }) 
+    // const meshCOF = new THREE.Mesh(geometryCOF, materialCOF);
+    // scene.add(meshCOF)
+
+    // const geometryAAPL = new THREE.SphereGeometry(data[0].currentPrice/100, 1,1);
+    // const materialAAPL = new THREE.MeshBasicMaterial({ color: 0xffff00, wireframe: true }) 
+    // const meshAAPL = new THREE.Mesh(geometryAAPL, materialAAPL);
+    // scene.add(meshAAPL)
+
+    // // camera.position.z = 10;
+
+    // const animate = () => {
+    //   requestAnimationFrame(animate);
+
+    //   meshCOF.rotation.x += 0.01;
+    //   meshCOF.rotation.y += 0.01;
+
+    //   renderer.render(scene, camera);
+    // }
+
+    // animate();
     
   }
 
+  
+
   render(){
     return (
-      <div>{ this.props.data.length >=1 ? this.three() : null }</div>
+      <div>{ this.props.data.length ? this.three() : null }
+      
+      </div>
     );
   }
 }
