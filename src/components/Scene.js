4528717481befe63ac5b4b613e727000980c2deb
KAZUE ANAN <zakscloset@gmail.com>
2018-07-31 20:50:54 -0400

    spheres;

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -73,23 +73,45 @@ class SomeScene extends Component {
 
     // renderer.render(scene, camera)
 
-    const geometryCOF = new THREE.SphereGeometry(data[2].currentPrice, 1, 1);
-    const materialCOF = new THREE.MeshBasicMaterial({ color: 0xffff00, wireframe: true }) 
+    const geometryCOF = new THREE.SphereGeometry(data[2].currentPrice, 8, 8);
+    const materialCOF = new THREE.MeshPhongMaterial({ color: 0xffff00, wireframe: true }) 
     const meshCOF = new THREE.Mesh(geometryCOF, materialCOF);
     meshCOF.position.x = data[2].currentPrice;
     scene.add(meshCOF)
 
-    const geometryAAPL = new THREE.SphereGeometry(data[0].currentPrice, 1, 1);
-    const materialAAPL = new THREE.MeshBasicMaterial({ color: 0xffff00, wireframe: true }) 
+    const geometryAAPL = new THREE.SphereGeometry(data[0].currentPrice, 10, 10);
+    const materialAAPL = new THREE.MeshPhongMaterial({ color: 0xffff00, wireframe: true }) 
     const meshAAPL = new THREE.Mesh(geometryAAPL, materialAAPL);
-    meshAAPL.position.x = data[0].currentPrice;
+    meshAAPL.position.x = data[0].currentPrice + 100;
+    meshAAPL.position.y = data[0].currentPrice - 250
     scene.add(meshAAPL)
 
-    const geometryGOOG = new THREE.SphereGeometry(data[1].currentPrice, 1, 1);
-    const materialGOOG = new THREE.MeshBasicMaterial({ color: 0xffff00, wireframe: true }) 
+    const geometryGOOG = new THREE.SphereGeometry(data[1].currentPrice, 18, 18);
+    const materialGOOG = new THREE.MeshPhongMaterial({ color: 0xffff00, wireframe: true }) 
     const meshGOOG = new THREE.Mesh(geometryGOOG, materialGOOG);
-    meshGOOG.position.x = data[1].currentPrice;
+    meshGOOG.position.x = data[1].currentPrice + 300;
     scene.add(meshGOOG)
+
+    const geometryMSFT = new THREE.SphereGeometry(data[4].currentPrice, 8, 8);
+    const materialMSFT = new THREE.MeshPhongMaterial({ color: 0xffff00, wireframe: true }) 
+    const meshMSFT = new THREE.Mesh(geometryMSFT, materialMSFT);
+    meshMSFT.position.x = data[4].currentPrice -100;
+    meshMSFT.position.y = data[4].currentPrice - 500
+    scene.add(meshMSFT)
+
+    const geometryC = new THREE.SphereGeometry(data[5].currentPrice, 8, 8);
+    const materialC = new THREE.MeshPhongMaterial({ color: 0xffff00, wireframe: true }) 
+    const meshC = new THREE.Mesh(geometryC, materialC);
+    meshC.position.x = data[5].currentPrice -300;
+    meshC.position.y = data[5].currentPrice - 700
+    scene.add(meshC)
+
+    const geometryZ = new THREE.SphereGeometry(data[6].currentPrice, 8, 8);
+    const materialZ = new THREE.MeshPhongMaterial({ color: 0xffff00, wireframe: true }) 
+    const meshZ = new THREE.Mesh(geometryZ, materialZ);
+    meshZ.position.x = data[6].currentPrice -500;
+    meshZ.position.y = data[6].currentPrice
+    scene.add(meshZ)
 
     // camera.position.z = 10;
 
@@ -102,8 +124,17 @@ class SomeScene extends Component {
       meshAAPL.rotation.x += 0.02;
       meshAAPL.rotation.y += 0.02;
 
-      meshGOOG.rotation.x += 0.02;
-      meshGOOG.rotation.y += 0.02;
+      meshGOOG.rotation.x += 0.005;
+      meshGOOG.rotation.y += 0.005;
+
+      meshMSFT.rotation.x += 0.02;
+      meshMSFT.rotation.y += 0.02;
+
+      meshC.rotation.x += 0.02;
+      meshC.rotation.y += 0.02;
+
+      meshZ.rotation.x += 0.02;
+      meshZ.rotation.y += 0.02;
 
       renderer.render(scene, camera);
     }
